32f141daf840437297c4b0ee49a03e6f237cab1f
yiyanglu1 <yiyang.lu@mail.utoronto.ca>
2023-10-01 19:10:38 -0400

    test api

--- .idea/Course_Project.java
+++ .idea/Course_Project.java
@@ -1,2 +1,16 @@
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import com.fasterxml.jackson.databind.ObjectMapper;
 public class Course_Project {
+    
+    
+
+    URL url = new URL("https://api.api-ninjas.com/v1/jokes?limit=3");
+    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+connection.setRequestProperty("accept", "application/json");
+    InputStream responseStream = connection.getInputStream();
+    ObjectMapper mapper = new ObjectMapper();
+    JsonNode root = mapper.readTree(responseStream);
+System.out.println(root.path("fact").asText());
 }
