1ee6288d1e0b6cc55a06095acfc49bee765781c8
johanxbh <johanxbh@gmail.com>
2023-11-21 12:41:22 -0500

    initial setup for the input data and controller for the new usecase list liked post

---
+++ src/interface_adapter/ListLikedPostController.java
@@ -0,0 +1,7 @@
+package use_case.check_like_post;
+import data_access.postDAO;
+
+public class CheckLikedPostController {
+    postDAO postdao;
+
+}
---
+++ src/use_case/list_liked_post/ListLikedPostInputData.java
@@ -0,0 +1,4 @@
+package use_case.check_like_post;
+
+public class CheckLikedPostInputData {
+}
