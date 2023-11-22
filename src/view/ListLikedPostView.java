8a31c2d39e2db4d915619024cfe968f872af0603
johanxbh <johanxbh@gmail.com>
2023-11-22 16:51:31 -0500

    initial set up for the view model for the usecase list liked post

--- src/view/ListLikedPostView.java
+++ src/view/ListLikedPostView.java
@@ -1,4 +1,19 @@
 package view;
 
-public class ListLikedPostView {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+public class ListLikedPostView extends JButton implements ActionListener, PropertyChangeListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+
+    }
 }
