fab3e5b69cb118c825a4ae1e9c03b705c3638e39
johanxbh <johanxbh@gmail.com>
2023-11-22 16:50:15 -0500

    add the fire property changed and add prperty change listener to this class also made the fireproperty change support for this viewmodel

--- src/interface_adapter/list_liked_post/ListLikedPostViewModel.java
+++ src/interface_adapter/list_liked_post/ListLikedPostViewModel.java
@@ -1,4 +1,32 @@
 package interface_adapter.list_liked_post;
 
-public class ListLikedPostViewModel {
+import interface_adapter.ViewModel;
+
+import javax.swing.text.View;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+public class ListLikedPostViewModel extends ViewModel {
+    private ListLikedPostState state = new ListLikedPostState();
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+    public ListLikedPostViewModel(String viewName) {
+        super("liked post");
+    }
+
+    @Override
+    public void firePropertyChanged() {
+        support.firePropertyChange("state", null, this.state);
+    }
+
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
+
+    public ListLikedPostState getState(){
+        return state;
+    }
+    public void setState(ListLikedPostState newstate){
+        this.state = newstate;
+    }
 }
