36859bc233c08801a50da50ae6146ebaaf808dce
johanxbh <johanxbh@gmail.com>
2023-11-22 16:48:57 -0500

    set the liked post viewmodel as an instance variable inside the corresponding presenter class

--- src/interface_adapter/list_liked_post/ListLikedPostPresenter.java
+++ src/interface_adapter/list_liked_post/ListLikedPostPresenter.java
@@ -4,6 +4,13 @@ import use_case.list_liked_post.ListLikedPostOutputBoundary;
 import use_case.list_liked_post.ListLikedPostOutputData;
 
 public class ListLikedPostPresenter implements ListLikedPostOutputBoundary {
+    private final ListLikedPostViewModel likedPostViewModel;
+
+
+    public ListLikedPostPresenter(ListLikedPostViewModel likedPostViewModel) {
+        this.likedPostViewModel = likedPostViewModel;
+    }
+
     @Override
     public void prepareSuccessView(ListLikedPostOutputData listLikedPostOutputData) {
 
