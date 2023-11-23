7216eb3de3f3ad5e864c4745ac938d34c0ed811b
johanxbh <johanxbh@gmail.com>
2023-11-22 20:32:15 -0500

    temperary setup for the ListLikedPostState

--- src/interface_adapter/list_liked_post/ListLikedPostState.java
+++ src/interface_adapter/list_liked_post/ListLikedPostState.java
@@ -1,4 +1,22 @@
 package interface_adapter.list_liked_post;
 
+import java.util.ArrayList;
+import entities.postEntity;
 public class ListLikedPostState {
+    private ArrayList<postEntity> listOfLikedPost = new ArrayList<postEntity>();
+    private String postError = null;
+    public ListLikedPostState(ListLikedPostState listLikedPostState){
+        this.listOfLikedPost = listLikedPostState.listOfLikedPost;
+    }
+    public ListLikedPostState(){}
+    public ArrayList<postEntity> getListOfLikedPostArray(){
+        return listOfLikedPost;
+    }
+
+    public void setPostError(String postError) {
+        this.postError = postError;
+    }
+    public String getPostError(){
+        return this.postError;
+    }
 }
