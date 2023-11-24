2273ea060beba32b4641c53240cd521160f4d58d
johanxbh <johanxbh@gmail.com>
2023-11-24 16:59:04 -0500

    complete the method prepare success view in the listlikedpostPresenter

--- src/interface_adapter/list_liked_post/ListLikedPostPresenter.java
+++ src/interface_adapter/list_liked_post/ListLikedPostPresenter.java
@@ -2,6 +2,8 @@ package interface_adapter.list_liked_post;
 
 import use_case.list_liked_post.ListLikedPostOutputBoundary;
 import use_case.list_liked_post.ListLikedPostOutputData;
+import entities.postEntity;
+import java.util.ArrayList;
 
 public class ListLikedPostPresenter implements ListLikedPostOutputBoundary {
     private final ListLikedPostViewModel likedPostViewModel;
@@ -13,7 +15,11 @@ public class ListLikedPostPresenter implements ListLikedPostOutputBoundary {
 
     @Override
     public void prepareSuccessView(ListLikedPostOutputData listLikedPostOutputData) {
-
+            ListLikedPostState listLikedPostState = this.likedPostViewModel.getState();
+            ArrayList<postEntity> listOfPost = listLikedPostOutputData.getListOfPost();
+            listLikedPostState.setListOfLikedPostArray(listOfPost);
+            this.likedPostViewModel.setState(listLikedPostState);
+            this.likedPostViewModel.firePropertyChanged();
     }
 
     @Override
