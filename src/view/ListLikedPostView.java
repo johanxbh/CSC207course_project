999b79c45cd9f2077ef5dd9a5350edc0215e1214
johanxbh <johanxbh@gmail.com>
2023-11-24 13:49:42 -0500

    create the initializer for the list liked view, which will initialize the post in a JPanel

--- src/view/ListLikedPostView.java
+++ src/view/ListLikedPostView.java
@@ -1,12 +1,54 @@
 package view;
 
+import entities.postEntity;
+import interface_adapter.back.BackController;
+import interface_adapter.list_liked_post.ListLikedPostController;
+import interface_adapter.list_liked_post.ListLikedPostViewModel;
+
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.util.ArrayList;
 
-public class ListLikedPostView extends JButton implements ActionListener, PropertyChangeListener {
+public class ListLikedPostView extends JPanel implements ActionListener, PropertyChangeListener {
+    public final String viewName = "list liked post";
+    private final ListLikedPostViewModel listLikedPostViewModel;
+    private final ListLikedPostController listLikedPostController;
+    private final BackController backController;
+    final JButton back;
+
+    public ListLikedPostView(ListLikedPostViewModel listLikedPostViewModel, ListLikedPostController listLikedPostController, BackController backController) {
+        this.listLikedPostViewModel = listLikedPostViewModel;
+        this.listLikedPostController = listLikedPostController;
+        this.backController = backController;
+        this.listLikedPostViewModel.addPropertyChangeListener(this);
+        JLabel title = new JLabel("Liked Post");
+        title.setAlignmentX(Component.CENTER_ALIGNMENT);
+        ArrayList<postEntity> likedPost = this.listLikedPostViewModel.getState().getListOfLikedPostArray();
+        JPanel postsPanel = new JPanel();
+        for (postEntity post : likedPost){
+            JPanel onePost = this.createPostPanel(post);
+            postsPanel.add(onePost);
+        }
+        JScrollPane postsScroll = new JScrollPane(postsPanel);
+        this.add(postsScroll);
+        back  = new JButton(this.listLikedPostViewModel.BACK_LABEL);
+        back.addActionListener(
+                new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent evt) {
+                        if (evt.getSource().equals(back)){
+                            backController.execute();
+                        }
+                    }
+                }
+        );
+
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
 
@@ -14,6 +56,10 @@ public class ListLikedPostView extends JButton implements ActionListener, Proper
 
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
+
+    }
+    private JPanel createPostPanel(postEntity post){
+        Object postInfo = post.getPostInfo();
 
     }
 }
