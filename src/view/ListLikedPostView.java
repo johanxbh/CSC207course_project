ec8710f2a4e2ea9499af7b78f5389a3c09e92987
johanxbh <johanxbh@gmail.com>
2023-11-26 05:52:46 -0500

    add the comment plaza after each post Panel, which can be used to manifest comment

--- src/view/ListLikedPostView.java
+++ src/view/ListLikedPostView.java
@@ -28,6 +28,7 @@ public class ListLikedPostView extends JPanel implements ActionListener, Propert
         this.listLikedPostViewModel.addPropertyChangeListener(this);
         JLabel title = new JLabel("Liked Post");
         title.setAlignmentX(Component.CENTER_ALIGNMENT);
+        this.add(title);
         ArrayList<postEntity> likedPost = this.listLikedPostViewModel.getState().getListOfLikedPostArray();
 
 
@@ -36,6 +37,9 @@ public class ListLikedPostView extends JPanel implements ActionListener, Propert
         for (postEntity post : likedPost) {
             JPanel onePost = this.createPostPanel(post);
             postsPanel.add(onePost);
+            JPanel oneCommentPlaza = this.createPostCommentPanel(post);
+            postsPanel.add(oneCommentPlaza);
+            postsPanel.add(Box.createVerticalStrut(60));
         }
         JScrollPane postsScroll = new JScrollPane(postsPanel);
         postsScroll.getViewport().setPreferredSize(new Dimension(600, 500));
@@ -66,10 +70,17 @@ public class ListLikedPostView extends JPanel implements ActionListener, Propert
     }
 
     private JPanel createPostPanel(postEntity post) {
+        String content = (String) post.getPostInfo();
+        int postPanelHeight;
+        if (content.length() > 200){
+            postPanelHeight = 200;
+        } else {
+            postPanelHeight = content.length() * 3;
+        }
         int postPanelWidth = 500; // Set your desired width
-        int postPanelHeight = 100;
         JPanel postPanel = new JPanel();
-        postPanel.setLayout(new BorderLayout());
+        postPanel.setAlignmentX(JPanel.LEFT_ALIGNMENT);
+        postPanel.setLayout(new BoxLayout(postPanel, BoxLayout.Y_AXIS));
 //
         postPanel.setPreferredSize(new Dimension(postPanelWidth, postPanelHeight));
         postPanel.setMinimumSize(new Dimension(postPanelWidth, postPanelHeight));
@@ -82,6 +93,41 @@ public class ListLikedPostView extends JPanel implements ActionListener, Propert
         contentTextArea.setEditable(false);
         JScrollPane contentScrollPane = new JScrollPane(contentTextArea);
         postPanel.add(contentScrollPane);
+
         return postPanel;
+    }
+
+
+    private JPanel createPostCommentPanel(postEntity post){
+        JTextArea contentTextArea = new JTextArea("-".repeat(50) + "below are the comment\n");
+        ArrayList<String> commentList = post.getPostComment();
+        int totalWords = 0;
+        for (String i: commentList){
+            totalWords += i.length();
+        }
+        JPanel commentPanel = new JPanel();
+        commentPanel.setAlignmentX(JPanel.LEFT_ALIGNMENT);
+        int panelWidth = 500;
+        int panelHeight;
+        if (totalWords > 100 ){
+            panelHeight = 100;
+        }else {
+            panelHeight = totalWords * 4;
+        }
+        commentPanel.setLayout(new BoxLayout(commentPanel, BoxLayout.Y_AXIS));
+        commentPanel.setPreferredSize(new Dimension(panelWidth, panelHeight));
+        commentPanel.setMinimumSize(new Dimension(panelWidth, panelHeight));
+        commentPanel.setMaximumSize(new Dimension(panelWidth, panelHeight));
+        for (String i: commentList){
+            contentTextArea.append(i + "\n" + "-".repeat(50));
+        }
+        contentTextArea.setWrapStyleWord(true);
+        contentTextArea.setLineWrap(true);
+        contentTextArea.setEditable(false);
+        JScrollPane scrollComment = new JScrollPane(contentTextArea);
+        commentPanel.add(scrollComment);
+        JButton comment = new JButton("Comment");
+        commentPanel.add(comment);
+        return commentPanel;
     }
 }
