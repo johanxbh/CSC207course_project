b7666c13710da36d9b300058cba63852ca67bc34
johanxbh <johanxbh@gmail.com>
2023-11-28 01:37:36 -0500

    first attemption to create the post plaza view model and the post state by adding a map of post and enable set of the post erro in the state, and add the name for the button in the view model

--- src/interface_adapter/post_plaza/PostPlazaViewModel.java
+++ src/interface_adapter/post_plaza/PostPlazaViewModel.java
@@ -1,4 +1,28 @@
 package interface_adapter.post_plaza;
 
-public class PostPlazaViewModel {
+import interface_adapter.ViewModel;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+public class PostPlazaViewModel extends ViewModel {
+    public static final String PULL_LABEL = "refresh post plaza";
+    public static final String CHECK_LIKED_POST_LABEL = "check liked post";
+    public static final String NEW_POST_LABEL = "new post";
+    private PostPlazaState state = new PostPlazaState();
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+
+    public PostPlazaViewModel(String viewName) {
+        super("post plaza");
+    }
+
+    @Override
+    public void firePropertyChanged() {
+        support.firePropertyChange("post plaza new", null, this.state);
+    }
+
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
 }
