a9180eac869feac75ff82eae5fecc6e33614e19a
johanxbh <johanxbh@gmail.com>
2023-11-28 03:05:33 -0500

    numerous update for the postPlazaView, create three method inside the view to create panel for the post, content, and button respectively and add them into the view

--- src/interface_adapter/post_plaza/PostPlazaViewModel.java
+++ src/interface_adapter/post_plaza/PostPlazaViewModel.java
@@ -9,10 +9,12 @@ public class PostPlazaViewModel extends ViewModel {
     public static final String PULL_LABEL = "refresh post plaza";
     public static final String CHECK_LIKED_POST_LABEL = "check liked post";
     public static final String NEW_POST_LABEL = "new post";
+    public static final String COMMENT_LABEL = "comment";
+    public static final String LIKE_LABEL = "like";
     private PostPlazaState state = new PostPlazaState();
     private final PropertyChangeSupport support = new PropertyChangeSupport(this);
 
-    public PostPlazaViewModel(String viewName) {
+    public PostPlazaViewModel() {
         super("post plaza");
     }
 
@@ -25,4 +27,12 @@ public class PostPlazaViewModel extends ViewModel {
     public void addPropertyChangeListener(PropertyChangeListener listener) {
         support.addPropertyChangeListener(listener);
     }
+    public PostPlazaState getState(){
+        return this.state;
+    }
+    public void setState(PostPlazaState postPlazaState){
+        this.state = postPlazaState;
+    }
+
+
 }
