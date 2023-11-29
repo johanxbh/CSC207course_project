edbed22173a6dddfdb50a91937da0e1c331c4a81
yiyanglu1 <yiyang.lu@mail.utoronto.ca>
2023-11-29 14:34:11 -0500

    try to solve conflict

--- src/interface_adapter/postState.java
+++ src/interface_adapter/postState.java
@@ -1,7 +1,10 @@
-package data_access;
+package interface_adapter;
 
 public class postState {
     private String postText;
+    private String postError;
+    private String postSuccess;
+    private String postInputText;
     public postState(){
     }
     public String getPostText(){
@@ -12,5 +15,26 @@ public class postState {
     }
     public void setPostText(String newPostText){
         this.postText = newPostText;
+    }
+
+    public void setPostError(String postError) {
+        this.postError = postError;
+    }
+
+    public String getPostError() {
+        return postError;
+    }
+    public String getPostSuccess(){
+        return postSuccess;
+    }
+    public void setPostSuccess(String c){
+        postSuccess = c;
+    }
+
+    public String getPostInputText() {
+        return postInputText;
+    }
+    public void setPostInputText(String text){
+        postInputText = text;
     }
 }
