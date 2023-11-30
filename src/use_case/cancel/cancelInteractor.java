b2a930526a682672b153f2f87a27afb07eb1a6eb
yiyanglu1 <yiyang.lu@mail.utoronto.ca>
2023-11-29 21:01:59 -0500

    create new cancelInteractor which will simply ask cencelPresenter to cancel the TextField

--- src/use_case/cancel/cancelInteractor.java
+++ src/use_case/cancel/cancelInteractor.java
@@ -1,2 +1,12 @@
-package use_case.cancel;public class cancelInteractor {
+package use_case.cancel;
+
+public class cancelInteractor implements cancelInputBoundary{
+    private cancelOutputBoundary cancelPresenter;
+    public cancelInteractor(cancelOutputBoundary cancelPresenter){
+        this.cancelPresenter = cancelPresenter;
+    }
+    @Override
+    public void execute() {
+        cancelPresenter.execute();
+    }
 }
