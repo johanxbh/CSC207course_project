ea4628030f0a225ccded5146d7c61dcc22f71119
yiyanglu1 <yiyang.lu@mail.utoronto.ca>
2023-11-29 20:59:18 -0500

    create new cancelPresenter which will be called by cancelInteractor to change the cancelViewModel

--- src/interface_adapter/cancel/cancelPresenter.java
+++ src/interface_adapter/cancel/cancelPresenter.java
@@ -1,2 +1,14 @@
-package interface_adapter.cancel;public class cancelPresenter {
+package interface_adapter.cancel;
+
+import use_case.cancel.cancelOutputBoundary;
+
+public class cancelPresenter implements cancelOutputBoundary {
+    private cancelViewModel cancelViewModel;
+    public cancelPresenter(cancelViewModel cancelViewModel){
+        this.cancelViewModel = cancelViewModel;
+    }
+    @Override
+    public void execute() {
+        cancelViewModel.firePropertyChanged();
+    }
 }
