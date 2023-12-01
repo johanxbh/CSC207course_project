b078dec8b1f2b8ccc89cd74aa275382595599179
johanxbh <johanxbh@gmail.com>
2023-11-30 21:04:12 -0500

    add a post data access object

--- src/data_access/postDataAccessObject.java
+++ src/data_access/postDataAccessObject.java
@@ -1,4 +1,46 @@
 package data_access;
 
-public class postDataObject {
+import entities.postEntity;
+
+import java.util.List;
+public class postDataAccessObject implements postDAO{
+    String API_TOKEN;
+    public postDataAccessObject(String api){
+        this.API_TOKEN = api;
+
+    }
+    @Override
+    public postEntity getMostRecentPost() {
+        return null;
+    }
+
+    @Override
+    public void savePost(postEntity post) {
+
+    }
+
+    @Override
+    public void cleanAllPost() {
+
+    }
+
+    @Override
+    public postEntity getMostPopularPost() {
+        return null;
+    }
+
+    @Override
+    public postEntity getPost(Integer postid) {
+        return null;
+    }
+
+    @Override
+    public List<postEntity> getLatestPosts() {
+        return null;
+    }
+
+    @Override
+    public postEntity getlatestPost(List<postEntity> posts) {
+        return null;
+    }
 }
