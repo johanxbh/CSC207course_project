e516c2daac85f471215052dff8f5658a3384cd5e
yiyanglu1 <yiyang.lu@mail.utoronto.ca>
2023-12-01 12:07:01 -0500

    change the attribute name postInfo to postText and add a new parameter postPicture

--- src/interface_adapter/post/postViewModel.java
+++ src/interface_adapter/post/postViewModel.java
@@ -1,12 +1,16 @@
-package interface_adapter;
+package interface_adapter.post;
+
+import interface_adapter.ViewModel;
+import interface_adapter.post.postState;
 
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 
 public class postViewModel extends ViewModel {
     public static final String POST_BUTTON_LABEL = "post";
+    public static final String PICTURE_BUTTON_LABEL = "selectPicture";
     public static final String LABLE_TITLE = "postview";
-    private postState postState = new postState();
+    private interface_adapter.post.postState postState = new postState();
     private final PropertyChangeSupport support = new PropertyChangeSupport(this);
     public postViewModel() {
         super("post");
