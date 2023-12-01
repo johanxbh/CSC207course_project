995162eec275397ac8e96dbb82c69ef82b74a4d5
yiyanglu1 <yiyang.lu@mail.utoronto.ca>
2023-12-01 12:08:32 -0500

    add a new attribute postPicture and the accesser method getPostPicture

--- src/use_case/post/PostinputData.java
+++ src/use_case/post/PostinputData.java
@@ -2,10 +2,16 @@ package use_case.post;
 
 public class PostinputData {
     private String postText;
-    public PostinputData(String text){
+    private String postPicture;
+    public PostinputData(String text, String postPicture){
+        this.postPicture = postPicture;
         postText = text;
     }
     public String getPostText(){
         return postText;
+    }
+
+    public String getPostPicture() {
+        return postPicture;
     }
 }
