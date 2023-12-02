3b3cb510797230a1f4d24c173d8144b6505f2c4f
johanxbh <johanxbh@gmail.com>
2023-12-02 17:56:23 -0500

    some changes related to the post Data Access Object

--- src/use_case/list_liked_post/ListLikedPostInteractor.java
+++ src/use_case/list_liked_post/ListLikedPostInteractor.java
@@ -1,14 +1,16 @@
 package use_case.list_liked_post;
+
 import data_access.postDAO;
+import entities.postEntity;
 import interface_adapter.list_liked_post.ListLikedPostInputData;
 
+import java.io.IOException;
 import java.util.ArrayList;
-import entities.postEntity;
 public class ListLikedPostInteractor implements ListLikedPostInputBoundary{
     private postDAO postdao;
     private ListLikedPostOutputBoundary listLikedPostPresenter;
     @Override
-    public void execute(ListLikedPostInputData inputData) {
+    public void execute(ListLikedPostInputData inputData) throws IOException {
         ArrayList<Integer> likedlist = inputData.getLikedPostList();
         if (likedlist.size() == 0){
             listLikedPostPresenter.prepareFailView("No liked post");
--- src/view/postView.java
+++ src/view/postView.java
@@ -1,10 +1,10 @@
 package view;
 
-import java.awt.image.BufferedImage;
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
-
+import interface_adapter.cancel.cancelController;
 import interface_adapter.cancel.cancelViewModel;
+import interface_adapter.post.postController;
+import interface_adapter.post.postState;
+import interface_adapter.post.postViewModel;
 
 import javax.swing.*;
 import java.awt.*;
@@ -12,13 +12,11 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.io.File;
 import java.io.IOException;
-
-import interface_adapter.cancel.cancelController;
-import interface_adapter.post.postController;
-import interface_adapter.post.postState;
-import interface_adapter.post.postViewModel;
 
 
 
@@ -112,7 +110,11 @@ public class postView extends JPanel implements PropertyChangeListener, ActionLi
                         if (evt.getSource().equals(post)) {
                             postState currentState = postViewModel.getState();
                             System.out.println("begin" + currentState.getPostPictureText());
-                            postController.execute(currentState.getPostInputText(), selectedImagePath);
+                            try {
+                                postController.execute(currentState.getPostInputText(), selectedImagePath);
+                            } catch (IOException e) {
+                                throw new RuntimeException(e);
+                            }
                         }
                     }
                 }
