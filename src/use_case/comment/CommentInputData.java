7db18ff8d2e5113059877eb67941859703f0018a
TomBrainiac <tomb.yang@mail.utoronto.ca>
2023-12-02 18:59:15 -0500

    CommentInputData

--- src/use_case/comment/CommentInputData.java
+++ src/use_case/comment/CommentInputData.java
@@ -1,10 +1,18 @@
 package use_case.comment;
 
+import entities.postEntity;
+
 public class CommentInputData {
+    private postEntity post;
     final private String comment;
 
-    public CommentInputData(String comment) {
+    public CommentInputData(postEntity post, String comment) {
+        this.post = post;
         this.comment = comment;
+    }
+
+    public postEntity getPost() {
+        return post;
     }
 
     public String getComment() {
