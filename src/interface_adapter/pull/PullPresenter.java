15cceae5fc328bcadf14516b86404c6d1e0943e1
lzh <zonghengli0107@gmail.com>
2023-12-02 22:58:54 -0500

    pull

--- src/interface_adapter/pull/PullPresenter.java
+++ src/interface_adapter/pull/PullPresenter.java
@@ -1,19 +1,26 @@
 package interface_adapter.pull;
 
+import interface_adapter.post_plaza.PostPlazaState;
 import use_case.pull.PullOutputBoundary;
 import use_case.pull.PullOutputData;
-import interface_adapter.pull.PullViewModel;
+import interface_adapter.post_plaza.PostPlazaViewModel;
 
 public class PullPresenter implements PullOutputBoundary {
     private final PullViewModel viewModel;
 
-    public PullPresenter(PullViewModel viewModel) {
+    private final PostPlazaViewModel postPlazaViewModel;
+
+    public PullPresenter(PullViewModel viewModel, PostPlazaViewModel postPlazaViewModel) {
         this.viewModel = viewModel;
+        this.postPlazaViewModel = postPlazaViewModel;
     }
 
     @Override
     public void presentNewPosts(PullOutputData outputData) {
         viewModel.setLatestPosts(outputData.getPosts());
+        PostPlazaState state = postPlazaViewModel.getState();
+        state.setPostList(outputData.getPosts());
+        postPlazaViewModel.firePropertyChanged();
         viewModel.notifyObservers(); // Notify the view that the data has been updated
     }
 }
